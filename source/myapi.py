c54a1f3e6496286007ae8e4146abd1f57dd02351
FilhoDeEd <102630043+FilhoDeEd@users.noreply.github.com>
2023-07-24 19:58:02 -0300

    Update myapi.py

--- source/myapi.py
+++ source/myapi.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
-@app.get("/")
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    },
+    2: {
+        "name": "Maria",
+        "age": 16,
+        "class": "year 11"
+    }
+}
+
+@app.get("/") #Isso é um endpoint
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}") #Isso é um endpoint com path parameter
+def get_student(student_id: int = Path(..., description = "The ID of the student you want to view", gt = 0, le = students.__len__())):
+    return students[student_id]
