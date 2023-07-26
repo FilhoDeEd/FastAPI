4b41ba21d20ceb321a4b13fcef6205d1aa172866
FilhoDeEd <102630043+FilhoDeEd@users.noreply.github.com>
2023-07-26 20:21:24 -0300

    Update myapi.py

--- source/myapi.py
+++ source/myapi.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,19 +8,50 @@ students = {
     1: {
         "name": "John",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     },
     2: {
         "name": "Maria",
         "age": 16,
-        "class": "year 11"
+        "year": "year 11"
     }
 }
 
-@app.get("/") #Isso é um endpoint
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+@app.get("/") # Isso é um endpoint
 def index():
     return {"name": "First Data"}
 
-@app.get("/get-student/{student_id}") #Isso é um endpoint com path parameter
-def get_student(student_id: int = Path(..., description = "The ID of the student you want to view", gt = 0, le = students.__len__())):
+@app.get("/get-student/{student_id}") # Isso é um endpoint com path parameter
+def get_student(student_id: int = Path(..., description = "The ID of the student you want to view", gt = 0, le = 50)):
+    return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(*, name: str = None, test: int):  # A typing permite você explicite parâmetros opcionais
+    for student_id in students.keys():
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+# O asterisco (*) antes de um parâmetro em uma função no Python indica que
+# esse parâmetro é um parâmetro de palavras-chave (keyword-only argument).
+# Isso significa que esse parâmetro só pode ser fornecido na chamada da função
+# usando a sintaxe de palavra-chave, e não como um argumento posicional.
+# Ex:
+    # func(*, a: int, b: int):
+    #   pass
+
+    # func(a = 1, b = 2) Ok
+    # func(1,2) Erro
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students.keys():
+        return {"Error": "Student already exists"}
+    
+    students[student_id] = student
     return students[student_id]
